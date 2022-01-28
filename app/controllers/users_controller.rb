67856910ef00762416252f63b24dd0ad65b934ad
Jenny Chau <jnechau@gmail.com>
2022-01-28 10:42:50 -0500

    jan 28 2022

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     def create #  post '/signup', to: 'users#create'
         user = User.create(user_params)
             if user.valid?
-                session[:user_id] = user.id
+                # session[:user_id] = user.id
                 render json: user, status: :ok
             else
                 render json: user.errors.full_messages, status: :unprocessable_entity
